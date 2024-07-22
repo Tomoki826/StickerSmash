bbad40e98c1fa78410b6bb4f07c9b035d35a6403
Tomoki826 <uda.tomoki@icloud.com>
2024-07-22 09:49:22 +0900

    画像の移動・拡大縮小

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import {
 } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import Button from './component/Button';
 import ImageViewer from './component/ImageViewer';
@@ -65,46 +66,48 @@ const App = () => {
 	const [pickedEmoji, setPickedEmoji] = useState<number | null>(null);
 
 	return (
-		<SafeAreaView style={styles.container}>
+		<GestureHandlerRootView>
 			<StatusBar style="inverted" />
-			<View style={styles.imageContainer}>
-				<ImageViewer
-					placeholderImageSource={PlaceholderImage}
-					selectedImage={selectedImage}
-				/>
-				{pickedEmoji && (
-					<EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
-				)}
-			</View>
-			<EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-				<EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
-			</EmojiPicker>
-			{showAppOptions ? (
-				<View style={styles.footerContainer}>
-					<View style={styles.optionsRow}>
-						<IconButton icon="refresh" label="Reset" onPress={onReset} />
-						<CircleButton onPress={onAddSticker} />
-						<IconButton
-							icon="save-alt"
-							label="Save"
-							onPress={onSaveImageAsync}
+			<SafeAreaView style={styles.container}>
+				<View style={styles.imageContainer}>
+					<ImageViewer
+						placeholderImageSource={PlaceholderImage}
+						selectedImage={selectedImage}
+					/>
+					{pickedEmoji && (
+						<EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+					)}
+				</View>
+				<EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+					<EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+				</EmojiPicker>
+				{showAppOptions ? (
+					<View style={styles.footerContainer}>
+						<View style={styles.optionsRow}>
+							<IconButton icon="refresh" label="Reset" onPress={onReset} />
+							<CircleButton onPress={onAddSticker} />
+							<IconButton
+								icon="save-alt"
+								label="Save"
+								onPress={onSaveImageAsync}
+							/>
+						</View>
+					</View>
+				) : (
+					<View style={styles.footerContainer}>
+						<Button
+							theme="primary"
+							label="Choose a photo"
+							onPress={pickImageAsync}
+						/>
+						<Button
+							label="Use this photo"
+							onPress={() => setShowAppOptions(true)}
 						/>
 					</View>
-				</View>
-			) : (
-				<View style={styles.footerContainer}>
-					<Button
-						theme="primary"
-						label="Choose a photo"
-						onPress={pickImageAsync}
-					/>
-					<Button
-						label="Use this photo"
-						onPress={() => setShowAppOptions(true)}
-					/>
-				</View>
-			)}
-		</SafeAreaView>
+				)}
+			</SafeAreaView>
+		</GestureHandlerRootView>
 	);
 };
 
--- component/EmojiSticker.tsx
+++ component/EmojiSticker.tsx
@@ -1,4 +1,10 @@
-import { View, Image } from 'react-native';
+import { View } from 'react-native';
+import Animated, {
+	useAnimatedStyle,
+	useSharedValue,
+	withSpring,
+} from 'react-native-reanimated';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 
 type PropsEmojiSticker = {
 	imageSize: number;
@@ -6,15 +12,63 @@ type PropsEmojiSticker = {
 };
 
 const EmojiSticker = ({ imageSize, stickerSource }: PropsEmojiSticker) => {
+	const scaleImage = useSharedValue(imageSize);
+
+	// ダブルタップで拡大操作
+	const doubleTap = Gesture.Tap()
+		.numberOfTaps(2)
+		.onStart(() => {
+			if (scaleImage.value !== imageSize * 2) {
+				scaleImage.value = imageSize * 2;
+			} else if (scaleImage.value === imageSize * 2) {
+				scaleImage.value = imageSize;
+			}
+		});
+
+	const imageStyle = useAnimatedStyle(() => {
+		return {
+			width: withSpring(scaleImage.value),
+			height: withSpring(scaleImage.value),
+		};
+	});
+
+	const translateX = useSharedValue(0);
+	const translateY = useSharedValue(0);
+
+	// ドラッグで移動
+	const drag = Gesture.Pan().onChange((event) => {
+		translateX.value += event.changeX;
+		translateY.value += event.changeY;
+	});
+
+	const containerStyle = useAnimatedStyle(() => {
+		return {
+			transform: [
+				{
+					translateX: translateX.value,
+				},
+				{
+					translateY: translateY.value,
+				},
+			],
+		};
+	});
+
 	return (
-		<View style={{ top: -350 }}>
-			<Image
-				source={stickerSource}
-				resizeMode="contain"
-				style={{ width: imageSize, height: imageSize }}
-			/>
-		</View>
+		<GestureDetector gesture={drag}>
+			<Animated.View style={[containerStyle, { top: -350 }]}>
+				<GestureDetector gesture={doubleTap}>
+					<Animated.Image
+						source={stickerSource}
+						resizeMode="contain"
+						style={imageStyle}
+					/>
+				</GestureDetector>
+			</Animated.View>
+		</GestureDetector>
 	);
 };
+
+// , { width: imageSize, height: imageSize } をanimate.imageにいる？
 
 export default EmojiSticker;
